c3f4490d349f90b88bbdeff8fc46250f4ce34d0d
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2023-10-29 23:11:40 +0530

    404 page added

---
+++ client/src/components/PageNotFound.js
@@ -0,0 +1,15 @@
+const PageNotFound=()=>{
+    return(
+     <>
+     <br/><br/><br/>
+     <div style={{textAlign:"center"}}>
+        <h1 style={{color:"aliceblue",fontSize:"50px",fontFamily:'Audiowide,Arial, Helvetica, sans-serif'}}>404 page not found</h1>
+        <img src="https://humornama.com/wp-content/uploads/2020/10/Bhaisahab-Ye-Kis-Line-Mei-Aa-Gaye-Aap-meme-template-of-welcome-movie-1024x576.jpg"
+       style={{width:"300px"}}
+       />
+     </div>
+     
+     </>
+    )
+}
+export default PageNotFound;

ce4de21a4a9b916fda6b1c75e2ee6ae4c129abca
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2023-10-30 23:47:47 +0530

    change config setting

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,12 @@
+import axios from "axios"
 const dpcount=19;
 const planetCount=21;
-export default {dpcount,planetCount}
+const id=JSON.parse(localStorage.getItem("id")).id
+
+const authAxios =axios.create({
+    baseURL:process.env.REACT_APP_API_ENDPOINT,
+    headers:{
+        Authorization:`Bearer ${id}`
+    }
+})
+export  {dpcount,planetCount,authAxios}

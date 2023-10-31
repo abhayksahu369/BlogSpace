1fe11e93fffbcf8a593c9e2fa2e3e52bc1688943
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2023-10-31 19:20:08 +0530

    changes in searchusercomponent

--- client/src/components/SearchUser.js
+++ client/src/components/SearchUser.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Nav from "./homepage/Nav";
+import Footer from "./homepage/Footer";
+import { Link } from "react-router-dom";
 
 const SearchUser=()=>{
     const[search,setSearch]=useState("")
@@ -13,33 +16,52 @@ const SearchUser=()=>{
     })
     
     useEffect(()=>{
-        const getusers=async()=>{
-            const result= await authAxios.get(`${process.env.REACT_APP_API_ENDPOINT}/user/getallusers`)
+        getusers()   
+    },[])
+    
+    const getusers=async()=>{
+        const result= await authAxios.get(`${process.env.REACT_APP_API_ENDPOINT}/user/getallusers`)
+        setUsers(result.data)
+        console.log(result.data)
+    }
+
+    const handleSearch=async(e)=>{
+        if(e.target.value){
+            const result=await authAxios.get(`${process.env.REACT_APP_API_ENDPOINT}/user/searchusers/${e.target.value}`)
             setUsers(result.data)
             console.log(result.data)
+        }else{
+            getusers()
         }
-        getusers()
-        
-    },[])
+        // if(e.target.value.length>0){
+        //    
+        // }
+
+    }
     return(
         <>
         <div className="searchInput">
-        <input type="text" placeholder="Search User" value={search}  />
+        <input type="text" placeholder="Search User"  onChange={handleSearch}  />
         </div>
+        <h1 className="spaceExplorers">Space Explorers</h1>
+
         <div className="usersList">
             {
                 users.length>0?
                 <>
                 {
                     users.map((items)=>
+                    <Link to={`/userprofile/${items._id}`}>
                         <div className="userContent" style={{color:"yellow"}} key={items._id}>
                         <img src= {`https://raw.githubusercontent.com/abhayksahu369/images/main/astronauts/astronaut${items.dpnumber}.png`} alt=""/>
                         <div className="usercontentRight">
-                            <h6>{items.username}</h6>
+                            <h5>{items.username}</h5>
                             <p>{items.name}</p>
                         </div>
                         </div>
+                        </Link>
                     )
+                    
                 }
 
                 </>:<></>
@@ -47,6 +69,7 @@ const SearchUser=()=>{
             }
             
         </div>
+        <Footer/>
         </>
     )
 }
